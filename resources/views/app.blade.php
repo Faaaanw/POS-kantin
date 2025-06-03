0a946971ca5c50fff275c7d093f79ccd55d69e0a
faaanw <akuafwan2@gmail.com>
2025-06-03 13:15:18 +0700

    update: mengganti nama aplikasi

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>SYBAU Mart</title>
+  <title>Lio Mart</title>
   <link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
   <link rel="stylesheet" href="{{asset('assets/css/layout.css')}}" />
   <link rel="stylesheet" href="{{asset('assets/css/sidebar.css')}}" />
